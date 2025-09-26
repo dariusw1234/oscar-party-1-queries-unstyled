096b3b3439a6e5fc8a30187eacf8691d819f3345
Darius Woods <dariuswoods1919@gmail.com>
2025-09-26 01:41:31 +0000

    added a dynamic route for a movie details page. used <dl> to display info about directors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  #MiscController
   get("/", { :controller => "misc", :action => "homepage" })
   get("/about", { :controller => "misc", :action => "about" })
+
+  #MoviesController
   get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
 end
